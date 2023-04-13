df08b54edf346a08a45672dd922960b12a88043f
davilivob <davilivob@gmail.com>
2023-04-13 18:34:24 +0800

    yeah yeah yeah

---
+++ src/osc-receiver.ts
@@ -0,0 +1,8 @@
+import { Server } from "node-osc";
+import "dotenv/config";
+const OSC_PORT = process.env.OSC_PORT_2;
+const OSC_IP = process.env.OSC_IP;
+
+export default new Server(OSC_PORT, OSC_IP, () => {
+  console.log(`OSC Server is listening on port ${OSC_PORT}`);
+});
